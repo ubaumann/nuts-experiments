ae797e4dde6bd6e706f68e8943a840ad6ab4fd67
ubaumann <github@m.ubaumann.ch>
2021-07-28 20:28:57 +0200

    Add register_assert_rewrite for plugin

--- nuts_experiments/__init__.py
+++ nuts_experiments/__init__.py
@@ -1,1 +1,6 @@
-__version__ = '0.1.0'
+__version__ = "0.1.0"
+
+import pytest
+
+# https://docs.pytest.org/en/stable/writing_plugins.html#assertion-rewriting
+pytest.register_assert_rewrite("nuts_experiments")
